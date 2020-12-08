e7bc3bc0dba84fb50beb33e5c4df9ac35efd7a6f
carlos romero <soportecarlosromero@gmail.com>
2020-12-08 16:43:50 -0500

    Cliente. implementa datepicker

--- src/app/admin/cliente/cliente-edit/cliente-edit.component.ts
+++ src/app/admin/cliente/cliente-edit/cliente-edit.component.ts
@@ -10,10 +10,41 @@ import { GeneralConfirmComponent } from 'app/admin/shared/components/general-con
 import { CLIENTE_CONSTANTS } from '../model/CLIENTE_CONSTANTS';
 import { ClienteService } from '../service/cliente.service';
 
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
+// Depending on whether rollup is used, moment needs to be imported differently.
+// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
+// syntax. However, rollup creates a synthetic default module and we thus need to import it using
+// the `default as` syntax.
+import * as _moment from 'moment';
+// import {default as _rollupMoment} from 'moment';
+// const moment = _rollupMoment || _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/yyyy',
+  },
+  display: {
+    dateInput: 'DD/MM/yyyy',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
+
 @Component({
   selector: 'app-cliente-edit',
   templateUrl: './cliente-edit.component.html',
-  styleUrls: ['./cliente-edit.component.css']
+  styleUrls: ['./cliente-edit.component.css'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+
+    {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS},
+  ],
 })
 export class ClienteEditComponent implements OnInit{
   roles: any = [];
